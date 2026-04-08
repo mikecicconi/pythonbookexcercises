a528dc0e66247a936d3b9c111275d481f6f65f0f
mikecicconi <bdsd77fpws@privaterelay.appleid.com>
2026-04-08 14:23:44 +0000

    2 more excercise files added

---
+++ fun_and_meth/say2.py
@@ -0,0 +1,13 @@
+def say(text):
+    print('==> ' + text)
+
+say('hello')
+say('hi')
+say('how do you do')
+say('Quite all right')
+
+  def greet(name):
+      print(f"Hello, {name}!")
+
+  greet("Alice")
+  greet("Bob")
